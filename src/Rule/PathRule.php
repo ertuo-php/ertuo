80cec5d56b1c02fce8151cfddb2af3353a25f6d6
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-12-21 16:20:42 +0200

    Adding Rule\PathRule

---
+++ src/Rule/PathRule.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ertuo\Rule;
+
+use Ertuo\Result;
+use Ertuo\RouteAwareInterface;
+use Ertuo\RouteAwareTrait;
+use Ertuo\Route;
+use Ertuo\Rule\CanHandleInterface;
+use Ertuo\Rule\RuleInterface;
+
+use function in_array;
+
+class PathRule implements RuleInterface, CanHandleInterface, RouteAwareInterface
+{
+	use RouteAwareTrait;
+
+	function isValid(string $key, array $until, Result $result) : bool
+	{
+		return !in_array($key, $until);
+	}
+
+	function handle(string $current, string $step, array $attributes, Result $result) : ?Route
+	{
+		$route = $this->getRoute();
+
+		if ($step && $route->name)
+		{
+			if (($current == $step) || empty($result->attributes[ $route->name ]))
+			{
+				$result->attributes[ $route->name ][] = $step;
+			}
+		}
+
+		if (!empty($attributes))
+		{
+			$result->attributes = $result->attributes
+				?  array_merge(
+					$result->attributes,
+					$attributes
+					)
+				: $attributes;
+		}
+
+		// last step, or the default kicking in...
+		//
+		if (!$step || $current != $step)
+		{
+			return $route->readRoute( '' );
+		}
+
+		return $route;
+	}
+}
---
+++ tests/Rule/WithDispatcher/PathRuleTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Ertuo\Tests\Rule\WithDispatcher;
+
+use Ertuo\Dispatcher;
+use Ertuo\RouteGroup as Route;
+use Ertuo\Rule\PathRule;
+use Ertuo\Rule\Aggregate as RuleAggregate;
+use PHPUnit\Framework\TestCase;
+
+class PathRuleTest extends TestCase
+{
+	protected $dispatcher;
+
+	function setUp()
+	{
+		$this->dispatcher = new Dispatcher(
+			Route::add('match')->rule('path', ['page'])->default('00000'),
+			(new RuleAggregate)->add('path', new PathRule)
+		);
+	}
+
+	function testPathEmpty()
+	{
+		$result = $this->dispatcher->dispatch([]);
+
+		$this->assertEquals($result->attributes, ['match' => '00000']);
+		$this->assertEquals($result->path, []);
+		$this->assertEquals($result->junk, []);
+	}
+
+	function testPath()
+	{
+		$result = $this->dispatcher->dispatch(['12', '34', '56']);
+
+		$this->assertEquals($result->attributes, ['match' => ['12', '34', '56']]);
+		$this->assertEquals($result->path, ['12', '34', '56']);
+		$this->assertEquals($result->junk, []);
+	}
+
+	function testpathWithUntil()
+	{
+		$result = $this->dispatcher->dispatch(['12', '34', 'page','56']);
+
+		$this->assertEquals($result->attributes, ['match' => ['12', '34']]);
+		$this->assertEquals($result->path, ['12', '34']);
+		$this->assertEquals($result->junk, ['page', '56']);
+	}
+}
